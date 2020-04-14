534ae808b10691c83d59eedff21a01ba43c43adc
Dean Attali <daattali@gmail.com>
2020-04-14 01:31:39 -0400

    unicafe step 5

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const FeedbackButton = ({setter, value, text}) => {
+const Button = ({setter, value, text}) => {
   const handler = () => setter(value + 1)
   return (
     <button onClick={handler}>{text}</button>
@@ -15,28 +15,43 @@ const Statistic = ({text, value, post}) => (
   </>
 )
 
+const Statistics = ({good, neutral, bad}) => {
+  const calculateAll = () => good + neutral + bad
+  const calculateAverage = () => (good - bad) / calculateAll()
+  const calculatePositive = () => good / calculateAll() * 100  
+  const noFeedback = () => calculateAll() === 0
+
+  if (calculateAll() === 0) {
+    return (
+      "No feedback given"
+    )
+  }
+
+  return (
+    <>
+    <Statistic value={good} text="good" />
+    <Statistic value={neutral} text="neutral" />
+    <Statistic value={bad} text="bad" />
+    <Statistic value={calculateAll()} text="all" />
+    <Statistic value={calculateAverage()} text="average" />
+    <Statistic value={calculatePositive()} text="positive" post="%" />
+    </>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const calculateAll = () => good + neutral + bad
-  const calculateAverage = () => (good - bad) / calculateAll()
-  const calculatePositive = () => good / calculateAll() * 100  
-
   return (
     <div>
       <h1>give feedback</h1>
-      <FeedbackButton value={good} setter={setGood} text="good" />
-      <FeedbackButton value={neutral} setter={setNeutral} text="neutral" />
-      <FeedbackButton value={bad} setter={setBad} text="bad" />
+      <Button value={good} setter={setGood} text="good" />
+      <Button value={neutral} setter={setNeutral} text="neutral" />
+      <Button value={bad} setter={setBad} text="bad" />
       <h1>statistics</h1>
-      <Statistic value={good} text="good" />
-      <Statistic value={neutral} text="neutral" />
-      <Statistic value={bad} text="bad" />
-      <Statistic value={calculateAll()} text="all" />
-      <Statistic value={calculateAverage()} text="average" />
-      <Statistic value={calculatePositive()} text="positive" post="%" />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
